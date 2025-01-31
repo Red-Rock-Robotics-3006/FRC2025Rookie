df85cff035241bd54a5e1f4c188c2c38b786f5e5
Yves Kloeckner <hosta.gruen@gmail.com>
2025-01-31 15:39:53 -0700

    singleton

---
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,30 @@
+import com.ctre.phoenix6.Utils;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.CoastOut;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Utils3006.SmartDashboardNumber;
+
+public class Elevator {
+    private static Elevator instance = null;
+
+    public static Elevator getInstance() {
+        if (Elevator.instance == null) Elevator.instance = new Elevator();
+        return Elevator.instance;
+    }
+}
